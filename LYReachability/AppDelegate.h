e9a478cabf319ff79ddf0ed63800bca092a9a655
zhangyoubo <13818956024@163.com>
2017-04-28 15:04:54 +0800

    Initial Commit

---
+++ LYReachability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYReachability
+//
+//  Created by 张友波 on 17/4/28.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYReachability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYReachability
+//
+//  Created by 张友波 on 17/4/28.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
